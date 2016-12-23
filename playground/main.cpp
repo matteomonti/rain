1e57913bd363a3a2561aeb7d41e806387fd7d326
Matteo Monti <matteo.monti@msoftprogramming.com>
2016-12-23 20:48:30 +0100

    Initial Commit

---
+++ playground/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  playground
+//
+//  Created by Matteo Monti on 23/12/16.
+//  Copyright © 2016 RAIN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
