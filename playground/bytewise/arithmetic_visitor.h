d8e8840804e89842c8a67a39b6bf33fdce43a19a
Matteo Monti <matteo.monti@msoftprogramming.com>
2016-12-25 01:43:17 +0100

    Ok, getting close to an arithmetic_visitor. Class ranges needs to be moved outside the visitor scope otherwise we will not be able to append them to each others. Also this doesn't handle arrays.

--- playground/bytewise/arithmetic_visitor.h
+++ playground/bytewise/arithmetic_visitor.h
@@ -2,7 +2,7 @@
 
 namespace bytewise
 {
-	class arithmetic_visitor;
+	template <typename> class arithmetic_visitor;
 };
 
 #if !defined(__forward__) && !defined(__playground__bytewise__arithmetic_visitor__h)
@@ -10,7 +10,7 @@ namespace bytewise
 
 namespace bytewise
 {
-	class arithmetic_visitor
+	template <typename atype> class arithmetic_visitor
 	{
   public:
     
@@ -41,6 +41,39 @@ namespace bytewise
     {
       typedef ranges <left_values..., right_values...> type;
     };
+    
+    template <ssize_t, bool> struct member_iterator;
+    
+    template <bool mdummy> struct member_iterator <-1, mdummy>
+    {
+      typedef ranges <> type;
+    };
+    
+    template <ssize_t index, bool mdummy> struct member_iterator
+    {
+      template <bool, bool, bool> struct conditional;
+      
+      template <bool, bool, bool> struct conditional
+      {
+        typedef ranges <> type;
+      };
+      
+      template <bool cdummy> struct conditional <true, false, cdummy>
+      {
+        typedef ranges <atype :: template __bytewise__ <index, false> :: offset(), sizeof(typename atype :: template __bytewise__ <index, false> :: type)> type;
+      };
+      
+      template <bool cdummy> struct conditional <false, true, cdummy>
+      {
+        typedef typename arithmetic_visitor <typename atype :: template __bytewise__ <index> :: type> :: type :: template shift <atype :: template __bytewise__ <index> :: offset()> type;
+      };
+      
+      typedef typename conditional <std :: is_arithmetic <typename atype :: template __bytewise__ <index, false> :: type> :: value, std :: is_class <typename atype :: template __bytewise__ <index, false> :: type> :: value, false> :: type member_type;
+      
+      typedef typename append <typename member_iterator <index - 1, false> :: type, member_type> :: type type;
+    };
+    
+    typedef typename member_iterator <count <atype> :: value - 1, false> :: type type;
 	};
 };
 
--- playground/main.cpp
+++ playground/main.cpp
@@ -5,8 +5,64 @@
 #include "data/buffer.hpp"
 #include "bytewise/arithmetic_visitor.hpp"
 
+/*class otherclass
+{
+  // Self
+  
+  typedef otherclass self;
+  
+  // Members
+  
+  buffer * a;
+  
+public:
+  
+  bytewise(a);
+};
+
+class myclass
+{
+  // Self
+  
+  typedef myclass self;
+  
+  // Members
+  
+  buffer a[12];
+  int b;
+  double c[44];
+  otherclass d;
+  
+public:
+  
+  bytewise(a);
+  bytewise(b);
+  bytewise(c);
+  bytewise(d);
+};*/
+
+class test
+{
+  // Self
+  
+  typedef test self;
+  
+  // Members
+  
+  int a;
+  int b;
+  double c;
+  
+public:
+  
+  // Bytewise
+  
+  bytewise(a);
+  bytewise(b);
+  bytewise(c);
+};
 
 int main()
 {
-  std :: cout << std :: is_same <bytewise :: arithmetic_visitor :: ranges <44, 2, 50, 4, 54, 2>, bytewise :: arithmetic_visitor :: append <bytewise :: arithmetic_visitor :: ranges <44, 2, 50, 4, 54, 2>, bytewise :: arithmetic_visitor :: ranges <>> :: type> :: value << std :: endl;
+  std :: cout << std :: is_same <bytewise :: arithmetic_visitor <test> :: ranges <0, 4, 4, 4, 8, 8>, bytewise :: arithmetic_visitor <test> :: type> :: value << std :: endl;
 }
