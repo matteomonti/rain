434d462ffcd67d3939c6d26b1ab669517c896aa8
Matteo Monti <matteo.monti@msoftprogramming.com>
2016-12-27 13:57:51 +0100

    Syncing repository.

---
+++ playground/bytewise/endianess.h
@@ -0,0 +1,19 @@
+// Forward declaration
+
+namespace bytewise
+{
+	class endianess;
+};
+
+#if !defined(__forward__) && !defined(__core__rs__monti__Desktop__rain__playground__playground__bytewise__endianess__h)
+#define __core__rs__monti__Desktop__rain__playground__playground__bytewise__endianess__h
+
+namespace bytewise
+{
+	class endianess
+	{
+
+	};
+};
+
+#endif
---
+++ playground/bytewise/endianess.hpp
@@ -0,0 +1,11 @@
+#ifndef __core__rs__monti__Desktop__rain__playground__playground__bytewise__endianess__hpp
+#define __core__rs__monti__Desktop__rain__playground__playground__bytewise__endianess__hpp
+
+#include "endianess.h"
+
+namespace bytewise
+{
+
+};
+
+#endif
