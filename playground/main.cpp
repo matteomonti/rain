2fb2fa2d9d3a896e87b6070f03c2597e30289db6
Matteo Monti <matteo.monti@msoftprogramming.com>
2017-02-03 18:09:28 +0100

    Backup commit.

--- playground/main.cpp
+++ playground/main.cpp
@@ -1,123 +1,86 @@
 #include <iostream>
 
-#include "bytewise/macro.hpp"
-#include "bytewise/arithmetic_visitor.hpp"
-#include "bytewise/buffer_scanner.hpp"
-#include "bytewise/buffer_visitor.hpp"
-#include "data/buffer.hpp"
+#include "macros/progressive.hpp"
 
-using namespace bytewise;
-
-template <size_t ...> struct path_iterator;
-
-template <size_t n, size_t ... ns> struct path_iterator <n, ns...>
+class assignment_proxy
 {
-  static void run()
-  {
-    std :: cout << n << "\t";
-    path_iterator <ns...> :: run();
-  }
 };
 
-template <> struct path_iterator <>
+template <typename type> void operator << (type &, assignment_proxy)
 {
-  static void run()
-  {
-    std :: cout << std :: endl;
-  }
-};
-
-template <size_t ... ns> void print_path(path <ns...>)
-{
-  path_iterator <ns...> :: run();
 }
 
-template <typename...> struct map_iterator;
-
-template <typename p, typename... ps> struct map_iterator <p, ps...>
+class transaction
 {
-  static void run()
-  {
-    print_path(p{});
-    map_iterator <ps...> :: run();
-  }
+public:
+  
+    int entrypoint = 0;
+    
+    virtual void run() = 0;
+    
+    assignment_proxy receive()
+    {
+        return assignment_proxy {};
+    }
+    
+    void wake()
+    {
+        this->run();
+    }
 };
 
-template <> struct map_iterator <>
+#define entry case 0
+
+#define async \
+bool __skip__ = false;\
+switch(this->entrypoint)
+
+#define await \
+this->entrypoint = __LINE__;\
+__skip__ = true;\
+case __LINE__:\
+if(__skip__) return
+
+class my_transaction : public transaction
 {
-  static void run()
-  {
-  }
+public:
+    
+    int my_int;
+    int my_other_int;
+    
+    void run()
+    {
+        async
+        {
+        entry:
+            
+            std :: cout << "Prima" << std :: endl;
+            await my_int << receive();
+            std :: cout << "Dopo" << std :: endl;
+            
+            for(my_other_int = 0; my_other_int < 10; my_other_int++)
+            {
+                std :: cout << my_other_int << std :: endl;
+                await my_int << receive();
+            }
+        }
+    }
 };
 
-template <typename... ps> void print_map(map <ps...>)
+int g()
 {
-  map_iterator <ps...> :: run();
+    return 3;
 }
 
-class myotherclass
+void non_ritoro_niente()
 {
-  // Self
-  
-  typedef myotherclass self;
-  
-public:
-  
-  // Members
-  
-  int a;
-  buffer b;
-  buffer c[2];
-  
-  // Bytewise
-  
-  bytewise(a);
-  bytewise(b);
-  bytewise(c);
-};
+}
 
-class myclass
+void devo_ritornare_void()
 {
-  // Self
-  
-  typedef myclass self;
-  
-public:
-  
-  // Members
-  
-  int a;
-  buffer b[3];
-  myotherclass c;
-  
-  // Bytewise
-  
-  bytewise(a);
-  bytewise(b);
-  bytewise(c);
-};
-
-class myvisitor
-{
-public:
-  
-  template <bool multiple> void buffer(const :: buffer & target)
-  {
-    std :: cout << "(multiple: " << multiple << ") " << target << std :: endl;
-  }
-};
+    return  /**/ g();
+}
 
 int main()
 {
-  myvisitor visitor;
-  myclass object;
-  
-  object.b[0] = "Il primo funziona!";
-  object.b[1] = "Il secondo funziona!";
-  object.b[2] = "Il terzo funziona!";
-  object.c.b = "Il quarto funziona!";
-  object.c.c[0] = "Il quinto funziona!";
-  object.c.c[1] = "Il sesto funziona!";
-  
-  buffer_visitor <myvisitor, myclass> :: visit(visitor, (const myclass &) object);
 }
